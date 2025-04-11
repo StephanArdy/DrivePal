4442ceb3a3771465bd6f79f203e139b7ce93daeb
stephan <ardistephan@gmail.com>
2025-04-11 08:18:10 +0800

    Initial Commit

---
+++ DrivePal/DrivePalApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrivePalApp.swift
+//  DrivePal
+//
+//  Created by stephan on 11/04/25.
+//
+
+import SwiftUI
+
+@main
+struct DrivePalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DrivePalTests/DrivePalTests.swift
@@ -0,0 +1,17 @@
+//
+//  DrivePalTests.swift
+//  DrivePalTests
+//
+//  Created by stephan on 11/04/25.
+//
+
+import Testing
+@testable import DrivePal
+
+struct DrivePalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
